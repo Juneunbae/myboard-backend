cf719410d7a522944c08072dca91fb54042dcc93
Juneunbae <dmsqo1403@naver.com>
2022-07-13 16:09:36 +0900

    DB

--- myboard/settings.py
+++ myboard/settings.py
@@ -85,7 +85,16 @@ WSGI_APPLICATION = 'myboard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = my_DATABASES
+DATABASES = {
+        'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'heroku_1fb473fb61eeaff',
+        'USER': 'b8157783cf0c3a',
+        'PASSWORD': '8c696e1e',
+        'HOST': 'us-cdbr-east-05.cleardb.net',
+        'PORT': '3306',
+    }
+}
 
 
 # Password validation
