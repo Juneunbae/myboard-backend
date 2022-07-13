bb6252a759bd5f99f9b14a48dc9a856e11d24975
Juneunbae <dmsqo1403@naver.com>
2022-07-13 16:12:44 +0900

    DB

--- myboard/settings.py
+++ myboard/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-from my_DB import my_DATABASES
+
+
 from pathlib import Path
 import os
 import environ
